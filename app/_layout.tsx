29442700ccac09046a5edc6807766a8df594bcce
tobias-carlbom <tobias@DESKTOP-72RUH6H>
2025-12-12 16:32:15 +0100

    app.json: default = portrait/landscape layout/index: hidden top and bottom android bar. app now runs in full screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setVisibilityAsync("hidden");
+      NavigationBar.setBehaviorAsync("overlay-swipe");
+    }
+  }, []);
+
+  return (
+    <>
+      <StatusBar hidden />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { View, useWindowDimensions } from "react-native";
+import { WebView } from "react-native-webview";
+import { useEffect, useRef, useState } from "react";
 
 export default function Index() {
+  const { width, height } = useWindowDimensions();
+  const prev = useRef({ width, height });
+  const [webKey, setWebKey] = useState(0);
+
+  /*
+  useEffect(() => {
+    const changed = prev.current.width !== width || prev.current.height !== height;
+    if (changed) {
+      prev.current = { width, height };
+      setTimeout(() => setWebKey((k) => k + 1), 1000);
+    }
+  }, [width, height]);
+  */
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }}>
+      <WebView key={webKey} source={{ uri: "http://172.31.0.30:1881" }} />
     </View>
   );
 }
