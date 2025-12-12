3a2a15d5b0e5b996845a72ad5cebe12626f90cff
tobias-carlbom <tobias@DESKTOP-72RUH6H>
2025-12-12 16:35:17 +0100

    no need for url refresh on orientation change. fuxa will handle this

--- app/index.tsx
+++ app/index.tsx
@@ -3,19 +3,7 @@ import { WebView } from "react-native-webview";
 import { useEffect, useRef, useState } from "react";
 
 export default function Index() {
-  const { width, height } = useWindowDimensions();
-  const prev = useRef({ width, height });
   const [webKey, setWebKey] = useState(0);
-
-  /*
-  useEffect(() => {
-    const changed = prev.current.width !== width || prev.current.height !== height;
-    if (changed) {
-      prev.current = { width, height };
-      setTimeout(() => setWebKey((k) => k + 1), 1000);
-    }
-  }, [width, height]);
-  */
 
   return (
     <View style={{ flex: 1 }}>
